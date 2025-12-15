0b295be24ccc543357be7d29525e0c1d7573a3b3
Bastián <bparedesp@falabella.cl>
2025-12-15 00:58:33 -0300

    dvf svs

--- src/app/screens/Camera.tsx
+++ src/app/screens/Camera.tsx
@@ -3,22 +3,47 @@ import { Text } from 'react-native';
 import { View, Button } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../components/navigation/Stack';
-/* import { useCameraPermission } from 'react-native-vision-camera' */
+import { useCameraPermission, useCameraDevice, Camera } from 'react-native-vision-camera'
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Camera'>;
 
 export function CameraScreen({ navigation }: Props) {
-  /* const { hasPermission, requestPermission } = useCameraPermission(); */
+  const device = useCameraDevice('front')
+  const { hasPermission, requestPermission } = useCameraPermission();
   useEffect(() => {
-    console.log('ayuda hasPermission');
+    if (!hasPermission) {
+      requestPermission();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return (
+
+  if (!hasPermission) return (
     <View>
       <Button
         title="Ir a Home"
         onPress={() => navigation.navigate('Home')}
       />
-      <Text>Contenido de Home</Text>
+      <Text>Contenido de Camera</Text>
+      <Text>User has permission {String(hasPermission)}</Text>
     </View>
   );
+
+  if (!device) return (
+    <View>
+      <Button
+        title="Ir a Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+      <Text>Contenido de Camera</Text>
+      <Text>User has permission {String(hasPermission)}</Text>
+    </View>
+  );
+
+  /* return <Text>A punto de mostrar camara</Text>; */
+
+  return <Camera
+    /* style={StyleSheet.absoluteFill} */
+    device={device}
+    isActive={true}
+  />
 }
